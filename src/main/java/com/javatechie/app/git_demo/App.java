fabedcd02eb78d982018dd38c9c81b8c544ab432
budime1205 <budime1708@gmail.com>
2019-07-24 18:27:08 +0530

    git-demo project to local repo

---
+++ src/main/java/com/javatechie/app/git_demo/App.java
@@ -0,0 +1,20 @@
+package com.javatechie.app.git_demo;
+
+/**
+ * Hello world!
+ *
+ */
+
+//budime1205, password1to9$
+public class App {
+
+    public void appData(String data){
+	   System.out.println("data"+data);
+      }
+
+
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
